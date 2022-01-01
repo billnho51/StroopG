64de0b98e4ce5e7a29bea49772bde659ce94681f
billnho51 <billnho1214@gmail.com>
2022-01-02 04:03:23 +1030

    added pause menu
    pause menu activate by pressing anywhere on the map except for the button

--- StroopGTest/Assets/Project/Scripts/gameplay.cs
+++ StroopGTest/Assets/Project/Scripts/gameplay.cs
@@ -6,20 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class gameplay : MonoBehaviour
 {
+    //public objects for indicating text to make changes to
     public TextMeshProUGUI largeText;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI finalScore;
-
     public GameObject durationTimer;
+    public GameObject gameUI;
+    public GameObject endUI;
+    public GameObject pauseUI;
 
     //private class for accessing and changing data
     private testingAnimate shaking;
     private DurationBar duration;
 
 
+    //keep track of score each play
     private int points;
     private bool timesUp;
     public static bool isPlaying = false;
+
+    public bool mouseOnobject = false;
 
 
 
@@ -27,8 +33,7 @@ public class gameplay : MonoBehaviour
     Color[] colours = new Color[4];
     //array for colors in text
     string[] colorNames = new string[4];
-    public GameObject gameUI;
-    public GameObject endUI;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -49,29 +54,23 @@ public class gameplay : MonoBehaviour
         colorNames[2] = "green";
         colorNames[3] = "yellow";
 
+        //set up activeUI
+        gameUI.SetActive(true);
+        pauseUI.SetActive(false);
+        endUI.SetActive(false);
+
         pickRandomColor();
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //update value each frame to check whether time has run out
         timesUp = duration.gameDone();
-
-        // if(timesUp == true)
-        // {
-
-        //     gameUI.SetActive(false);
-        //     endUI.SetActive(true);
-        //     //set final score
-        //     finalScore.text = points.ToString();
-        
-            
-
-        // }
-
-
-        if (Input.GetKeyDown(KeyCode.P))
+        //check if mouse is clicked anywhere except buttons
+        if (Input.GetMouseButtonDown(0) && !mouseOnobject)
         {
             if(isPlaying)
             {
@@ -90,7 +89,7 @@ public class gameplay : MonoBehaviour
     void Resume()
     {
         gameUI.SetActive(false);
-        //Time.timeScale = 1f;
+        pauseUI.SetActive(true);
         isPlaying = false;
 
     }
@@ -98,7 +97,7 @@ public class gameplay : MonoBehaviour
     void Pause()
     {
         gameUI.SetActive(true);
-        //Time.timeScale = 0f;
+        pauseUI.SetActive(false);
         isPlaying = true;
         
     }
@@ -120,6 +119,17 @@ public class gameplay : MonoBehaviour
         Debug.Log("Quitting game....");
         //does not quit current play in unity editor
         Application.Quit();
+    }
+
+
+    public void OnMouseEnter() 
+    {
+        mouseOnobject = true;     
+    }
+
+    public void OnMouseExit() 
+    {
+        mouseOnobject = false;    
     }
 
     // game play funtions
